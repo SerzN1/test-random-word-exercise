df2b2da7f99c23c677b50a417e299fb5c8598964
serzn1 <bob_s@list.ru>
2021-04-08 00:55:28 +0200

    Simplify solution, add unit tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,205 @@
+jest.mock("word-maker", () => ({
+    getRandomWord: jest.fn(),
+    getRandomWordSync: jest.fn(),
+}));
+
+import * as fs from "fs";
+import * as path from "path";
 import { getRandomWord, getRandomWordSync } from "word-maker";
-import { task1 } from "./index";
+import {
+    COUNT,
+    createItems,
+    getFizzBuzzWord,
+    getRandomItemSyncWithError,
+    getRandomItemWithError,
+    RES_FILE_NAME,
+    task1,
+    task2,
+    task3FizzBuzz,
+    task3NoFizzBuzz,
+    task4FizzBuzz,
+    task4NoFizzBuzz,
+} from "./index";
 
-describe("task 1", () => {
-    let consoleSpy;
+jest.mock("fs");
 
-    beforeEach(() => {
-        consoleSpy = jest.spyOn(console, "log");
+describe("Words generators", () => {
+    let consoleObjectLink;
+    const cb = () => ({});
+
+    beforeAll(() => {
+        consoleObjectLink = console;
+        console = {
+            log: cb,
+        } as unknown as Console;
+        (fs.writeFileSync as jest.Mock<any>).mockClear();
     });
 
-    it("should print 100 items into console", () => {
-        task1();
-        expect(consoleSpy).toHaveBeenCalledTimes(100);
+    afterAll(() => {
+        console = consoleObjectLink;
     });
+
+    it("should generate and save report to a file", () => {
+        expect((fs.writeFileSync as jest.Mock<any>).mock.calls).toHaveLength(1);
+        expect((fs.writeFileSync as jest.Mock<any>).mock.calls[0][0]).toBe(path.join(__dirname, RES_FILE_NAME));
+    });
+
+
+    describe("createItems", () => {
+        it("should generate items", () => {
+            const cb = jest.fn();
+            const res = createItems(10, cb);
+            expect(res).toHaveLength(10);
+            expect(cb).toHaveBeenCalledTimes(10);
+        });
+    });
+
+    describe("getFizzBuzzWord", () => {
+        it("should handle fizzbuzz cases", () => {
+            expect(getFizzBuzzWord(15)).toBe("FizzBuzz");
+            expect(getFizzBuzzWord(3)).toBe("Fizz");
+            expect(getFizzBuzzWord(5)).toBe("Buzz");
+            expect(getFizzBuzzWord(2)).toBeUndefined();
+            expect(getFizzBuzzWord(1)).toBeUndefined();
+            expect(getFizzBuzzWord(8)).toBeUndefined();
+        });
+    });
+
+    describe("getRandomItemSyncWithError", () => {
+        it("should generate random word in case of no errors", () => {
+            getRandomWordSync.mockReturnValueOnce("random word");
+            expect(getRandomItemSyncWithError()).toBe("random word");
+        });
+        it("should return fallback in case of error", () => {
+            getRandomWordSync.mockImplementationOnce(() => {
+                throw new Error();
+            });
+            expect(getRandomItemSyncWithError()).toBe("It shouldn't break anything!");
+        });
+    });
+
+    describe("getRandomItemWithError", () => {
+        it("should generate random word in case of no errors", () => {
+            getRandomWord.mockResolvedValueOnce("random word");
+            return getRandomItemWithError().then((res) => {
+                expect(res).toBe("random word");
+            });
+        });
+        it("should return fallback in case of error", () => {
+            getRandomWord.mockRejectedValueOnce(() => {
+                throw new Error();
+            });
+            return getRandomItemWithError().then((res) => {
+                expect(res).toBe("It shouldn't break anything!");
+            });
+        });
+    });
+
+    describe("Task 1", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+
+        it(`should generate ${COUNT} random words`, () => {
+            const res = task1();
+            expect(getRandomWordSync).toHaveBeenCalledTimes(COUNT);
+            expect(res).toHaveLength(COUNT);
+        });
+    });
+
+    describe("Task 2", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+
+        it(`should generate ${COUNT} random and fizzbuzz words`, () => {
+            const res = task2();
+            expect(res).toHaveLength(COUNT);
+            expect(getRandomWordSync).toHaveBeenCalled();
+            expect(res[14]).toBe("FizzBuzz");
+            expect(res[4]).toBe("Buzz");
+            expect(res[2]).toBe("Fizz");
+        });
+    });
+
+    describe("Task 3 without FizzBuzz", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+        it(`should generate ${COUNT} random words asynchronously`, () => {
+            return task3NoFizzBuzz().then((res) => {
+                expect(res).toHaveLength(COUNT);
+                expect(getRandomWord).toHaveBeenCalledTimes(COUNT);
+            });
+        });
+    });
+
+    describe("Task 3 with FizzBuzz", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+        it(`should generate ${COUNT} random and fizzbuzz words asynchronously`, () => {
+            return task3FizzBuzz().then((res) => {
+                expect(res).toHaveLength(COUNT);
+                expect(getRandomWord).toHaveBeenCalled();
+                expect(res[14]).toBe("FizzBuzz");
+                expect(res[4]).toBe("Buzz");
+                expect(res[2]).toBe("Fizz");
+            });
+        });
+    });
+
+    describe("Task 4 without FizzBuzz", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+        it(`should generate ${COUNT} random words synchronously`, () => {
+            getRandomWordSync.mockResolvedValue("random word");
+            return task4NoFizzBuzz().then((res) => {
+                expect(res).toHaveLength(COUNT);
+                expect(getRandomWordSync).toHaveBeenCalledTimes(COUNT);
+                expect(res.every(((r) => r === "random word"))).toBe(true);
+            });
+        });
+        it(`should generate ${COUNT} random words synchronously with error fallback`, () => {
+            getRandomWordSync.mockImplementation(() => {
+                throw new Error("Random error");
+            });
+            return task4NoFizzBuzz().then((res) => {
+                expect(res).toHaveLength(COUNT);
+                expect(getRandomWordSync).toHaveBeenCalledTimes(COUNT);
+                expect(res.every(((r) => r === "It shouldn't break anything!"))).toBe(true);
+            });
+        });
+    });
+
+    describe("Task 4 with FizzBuzz", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+        it(`should generate ${COUNT} random words and fizzbuzz asynchronously`, () => {
+            getRandomWord.mockResolvedValue("random word");
+            return task4FizzBuzz().then((res) => {
+                expect(res).toHaveLength(COUNT);
+                expect(getRandomWord).toHaveBeenCalled();
+                expect(res[14]).toBe("FizzBuzz");
+                expect(res[4]).toBe("Buzz");
+                expect(res[2]).toBe("Fizz");
+            });
+        });
+        it(`should generate ${COUNT} random words and fizzbuzz asynchronously with error fallback`, () => {
+            getRandomWord.mockImplementation(() => {
+                throw new Error("Random error");
+            });
+            return task4FizzBuzz().then((res) => {
+                expect(res).toHaveLength(COUNT);
+                expect(getRandomWord).toHaveBeenCalled();
+                expect(res.some(((r) => r === "It shouldn't break anything!"))).toBe(true);
+                expect(res[14]).toBe("FizzBuzz");
+                expect(res[4]).toBe("Buzz");
+                expect(res[2]).toBe("Fizz");
+            });
+        });
+    });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,36 @@
-// tslint:disable no-console
+/// <reference types="node" />
+
+import * as fs from "fs";
+import * as path from "path";
+
 import { getRandomWord, getRandomWordSync } from "word-maker";
 
-console.log("It works!");
+export const COUNT = 100;
+export const RES_FILE_NAME = "results.json";
 
-// Task 1
-export function task1() {
-    const printRandomItemSync = (i: number) => {
-        console.log(`${i + 1}. ${getRandomWordSync()}`);
-    };
-    // Need to separate data generation and logging for sending
-    return doTimesSync(100, printRandomItemSync);
-}
+// 1. Print numbers from 1 to 100 to the console, but for each number also
+// print a random word using the function `getRandomWordSync`. E.g.
+//
+// 1: four
+// 2: firm
+// 3: shape
+// 4: choice
+// 5: coach
+// 6: purple
+// ...
+// 100: buffalo
 
-// Task 2
-export function task2() {
-    function printRandomOrAndFizzBuzzItemSync(i: number) {
-        const result = checkFizzBuzz(i + 1) || getRandomWordSync();
-        console.log(`${i + 1}. ${result}`);
-    }
-    return doTimesSync(100, printRandomOrAndFizzBuzzItemSync);
-}
+export const createItems = (count: number, cb: (_: any, i: number) => any) =>
+    new Array(count).fill(undefined).map(cb);
 
-// Task 3 Without fizz buzz
-export function task3NoFizzBuzz() {
-    const createTask = () => getRandomWord();
-    return doTimes(100, createTask, printResults);
-}
+export const task1 = () => createItems(COUNT, getRandomWordSync);
 
-// Task 3 With fizz buzz
-export function task3FizzBuzz() {
-    const createTask = (i) => getRandomOrFizzBuzzItem(i);
-    return doTimes(100, createTask, printResults);
-}
+// 2. Modify your code to be a "Fizz Buzz" program. That is, print the numbers
+//  as in the previous step, but for multiples of three, print "Fizz"
+// (instead of the random word), for multiples of five, print "Buzz"
+// and for numbers which are both multiples of three and five, print "FizzBuzz".
 
-// Task 4 Without fizz buzz
-export function task4NoFizzBuzz() {
-    const createTask = (i) => getRandomItemWithError(i);
-    return doTimes(100, createTask, printResults);
-}
-
-// Task 4 With fizz buzz
-export function task4FizzBuzz() {
-    const createTask = (i) => getRandomOrFizzBuzzItemWithError(i);
-    return doTimes(100, getRandomOrFizzBuzzItemWithError, printResults);
-}
-
-(function sendItems() {
-    Promise.all([
-        task1(),
-        task2(),
-        task3NoFizzBuzz(),
-        task3FizzBuzz(),
-        task4NoFizzBuzz(),
-        task4FizzBuzz(),
-    ])
-    .then((data) => fetch("/api/postExample", {
-        body: JSON.stringify(data),
-        method: "POST",
-    }))
-    .catch((err) => console.log("My bad"));
-})();
-
-function doTimesSync(times = 0, cb: (i: number) => void) {
-    for (let i = 0; i < times; i += 1) {
-        cb(i);
-    }
-}
-
-function doTimes(
-    times = 100,
-    taskCreator: (index: number) => Promise<any>,
-    cb: (items: number|string[]) => void,
-) {
-    const tasksArray = [];
-    doTimesSync(100, (i) => tasksArray.push(taskCreator(i)));
-    return Promise.all(tasksArray).then(printResults);
-}
-
-function checkFizzBuzz(i: number): string | undefined {
+export function getFizzBuzzWord(i: number): string | undefined {
     if (i % 3 === 0 && i % 5 === 0) {
         return "FizzBuzz";
     } else if (i % 3 === 0) {
@@ -87,35 +40,81 @@ function checkFizzBuzz(i: number): string | undefined {
     }
 }
 
-async function getRandomOrFizzBuzzItem(i) {
-    let result = checkFizzBuzz(i + 1);
-    if (!result) {
-        result = await getRandomWord();
-    }
-    return result;
-}
+export const task2 = () => createItems(COUNT, (_, i) => getFizzBuzzWord(i + 1) || getRandomWordSync());
 
-async function getRandomItemWithError(i) {
+// 3. Create a version of steps *1* and *2* using the **asynchronous**
+// function, `getRandomWord`. This function returns a Promise, which resolves
+// to a random word string. Ideally, the numbers should be in numerical order.
+
+export const task3NoFizzBuzz = () => Promise.all(createItems(COUNT, getRandomWord));
+export const task3FizzBuzz = () => Promise.all(createItems(COUNT, (_, i) => getFizzBuzzWord(i + 1) || getRandomWord()));
+
+// 4. Add error handling to both the synchronous and asynchronous solutions
+// (calling `getRandomWord({ withErrors: true })` will intermitently throw
+// an error instead of return a random word). When an error is caught,
+// the program should print "It shouldn't break anything!" instead
+// of the random word, "Fizz", "Buzz" or "FizzBuzz"
+
+export const getRandomItemSyncWithError = (): string => {
     try {
-        const result = await getRandomWord({ withErrors: true });
-        return result;
+        return getRandomWordSync({ withErrors: true });
     } catch (e) {
-        return e;
+        return "It shouldn't break anything!";
     }
+};
+
+export const task4NoFizzBuzz = () =>
+    Promise.all(createItems(COUNT, getRandomItemSyncWithError));
+
+export const getRandomItemWithError = async (): Promise<string> => {
+    try {
+        const word = await getRandomWord({ withErrors: true });
+        return word;
+    } catch (e) {
+        return "It shouldn't break anything!";
+    }
+};
+
+export const task4FizzBuzz = () =>
+    Promise.all(createItems(COUNT, (_, i) => getFizzBuzzWord(i + 1) || getRandomItemWithError()));
+
+// 5. For **Node.JS developers**: Instead of printing the console.
+// Write the information to a file in the root of this project.
+// For **Frontend** developers, send your result to an HTTP endpoint
+// (since there is no running endpoint, this part of your solution
+// does not need to actually run)
+
+(async function sendItems() {
+    const res = await Promise.all([
+        task1(),
+        task2(),
+        task3NoFizzBuzz(),
+        task3FizzBuzz(),
+        task4NoFizzBuzz(),
+        task4FizzBuzz(),
+    ]);
+
+    printResults(res);
+
+    try {
+        fetch("/api/postExample", {
+            body: JSON.stringify(res),
+            method: "POST",
+        });
+    } catch (e) {
+        saveResults(res, RES_FILE_NAME);
+    }
+})();
+
+function printResults(res: string[][]): void {
+    res.forEach((r) => {
+        // tslint:disable-next-line no-console
+        console.log("\n");
+        // tslint:disable-next-line no-console
+        console.log(r.map((item, i) => `${i + 1}. ${item}`).join("\n"));
+    });
 }
 
-async function getRandomOrFizzBuzzItemWithError(i) {
-    let result = checkFizzBuzz(i + 1);
-    if (!result) {
-        result = await getRandomItemWithError(i);
-    }
-    return result;
-}
-
-function printResults(results) {
-    const printResult = (result, i) => {
-        console.log(`${i + 1}. ${result}`);
-    };
-    results.forEach(printResult);
-    return results; // for chaining
+function saveResults(res: any, filename: string): void {
+    fs.writeFileSync(path.join(__dirname, filename), res.toString());
 }
